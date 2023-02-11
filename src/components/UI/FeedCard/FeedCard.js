130b6efd07c17f7ae3b0295c8a4773360ebadc6d
yww0225 <yw0225.career@gmail.com>
2023-02-10 21:00:11 -0800

    CF-41 implemented failure message if the like operation failed

--- src/components/UI/FeedCard/FeedCard.js
+++ src/components/UI/FeedCard/FeedCard.js
@@ -14,6 +14,8 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MapsUgcIcon from '@mui/icons-material/MapsUgc';
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
 import { pink } from '@mui/material/colors';
 
 import { CardStyled, ExpandMore } from './FeedCardStyles';
@@ -36,14 +38,32 @@ const FeedCard = (props) => {
   const [expanded, setExpanded] = useState(false);
   const [feedLikeStatus, setFeedLikeStatus] = useState(liked);
   const [disableLikeBtn, setDisableLikeBtn] = useState(false);
+  const [showFailMessage, setShowFailMessage] = useState(false);
 
   const expandClickHandler = () => {
     setExpanded(!expanded);
   };
 
+  const closeSnackBarHandler = () => {
+    setShowFailMessage(false);
+  };
+
+  const vertical = 'bottom';
+  const horizontal = 'center';
+
+  const closeAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      onClick={closeSnackBarHandler}
+    >
+      <CloseIcon fontSize="small" />
+    </IconButton>
+  );
+
   const changeLikeStatusHandler = () => {
-    console.log(feedLikeStatus);
-    setFeedLikeStatus(feedLikeStatus == 0 ? 1 : 0);
+    setFeedLikeStatus(feedLikeStatus === '0' ? '1' : '0');
     setDisableLikeBtn(true);
     setTimeout(() => {
       setDisableLikeBtn(false);
@@ -76,99 +96,112 @@ const FeedCard = (props) => {
       console.log(data);
     } catch (error) {
       console.log(error.message);
+      setShowFailMessage(true);
     }
   }
 
   return (
-    <CardStyled>
-      <CardHeader
-        avatar={
-          <Avatar
-            src={avatar}
-            sx={{ width: 56, height: 56 }}
-            alt="user avatar"
-          />
-        }
-        title={title}
-        titleTypographyProps={{
-          fontSize: 25,
-          color: '#616161',
-          marginLeft: 3,
-        }}
-        subheader={userId}
-        subheaderTypographyProps={{
-          fontSize: 17,
-          color: '#616161',
-          marginLeft: 3,
-        }}
-      />
-      {media.length === 1 && (
-        <CardMedia
-          component="img"
-          image={media[0].imgUrl}
-          style={{ maxHeight: '700px' }}
-          alt="post img"
-        />
-      )}
-
-      {media.length > 1 && (
-        <ImageList
-          sx={{
-            width: 1050,
-            maxHeight: 1500,
+    <>
+      <CardStyled>
+        <CardHeader
+          avatar={
+            <Avatar
+              src={avatar}
+              sx={{ width: 56, height: 56 }}
+              alt="user avatar"
+            />
+          }
+          title={title}
+          titleTypographyProps={{
+            fontSize: 25,
+            color: '#616161',
+            marginLeft: 3,
           }}
-          variant="masonry"
-          cols={2}
-          gap={8}
-        >
-          {media.map((item, index) => (
-            <ImageListItem key={index} sx={{ maxWidth: 675, maxHeight: 750 }}>
-              <img src={item.imgUrl} loading="lazy" alt="post img" />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      )}
+          subheader={userId}
+          subheaderTypographyProps={{
+            fontSize: 17,
+            color: '#616161',
+            marginLeft: 3,
+          }}
+        />
+        {media.length === 1 && (
+          <CardMedia
+            component="img"
+            image={media[0].imgUrl}
+            style={{ maxHeight: '700px' }}
+            alt="post img"
+          />
+        )}
 
-      <CardContent>
-        <Typography align="right" variant="body1" color="text.secondary">
-          {`${region} · ${date} `}
-        </Typography>
-      </CardContent>
+        {media.length > 1 && (
+          <ImageList
+            sx={{
+              width: 1050,
+              maxHeight: 1500,
+            }}
+            variant="masonry"
+            cols={2}
+            gap={8}
+          >
+            {media.map((item, index) => (
+              <ImageListItem key={index} sx={{ maxWidth: 675, maxHeight: 750 }}>
+                <img src={item.imgUrl} loading="lazy" alt="post img" />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        )}
 
-      <CardContent>
-        <Typography style={{ wordWrap: 'break-word' }} color="text.secondary">
-          {content}
-        </Typography>
-      </CardContent>
-
-      <CardActions disableSpacing>
-        <IconButton
-          disableFocusRipple
-          disableRipple
-          aria-label="add to favorites"
-          onClick={changeLikeStatusHandler}
-          style={{ position: 'absolute', left: '1110px' }}
-          sx={{ color: feedLikeStatus == 1 ? pink[500] : 'action' }}
-          disabled={disableLikeBtn}
-        >
-          <FavoriteIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={expandClickHandler}
-          aria-expanded={expanded}
-          aria-label="see and send comments"
-        >
-          <MapsUgcIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography color="text.secondary">comments will be here</Typography>
+          <Typography align="right" variant="body1" color="text.secondary">
+            {`${region} · ${date} `}
+          </Typography>
         </CardContent>
-      </Collapse>
-      {props.children}
-    </CardStyled>
+
+        <CardContent>
+          <Typography style={{ wordWrap: 'break-word' }} color="text.secondary">
+            {content}
+          </Typography>
+        </CardContent>
+
+        <CardActions disableSpacing>
+          <IconButton
+            disableFocusRipple
+            disableRipple
+            aria-label="add to favorites"
+            onClick={changeLikeStatusHandler}
+            style={{ position: 'absolute', left: '1110px' }}
+            sx={{ color: feedLikeStatus === '1' ? pink[500] : 'action' }}
+            disabled={disableLikeBtn}
+          >
+            <FavoriteIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={expandClickHandler}
+            aria-expanded={expanded}
+            aria-label="see and send comments"
+          >
+            <MapsUgcIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography color="text.secondary">
+              comments will be here
+            </Typography>
+          </CardContent>
+        </Collapse>
+        {props.children}
+      </CardStyled>
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={showFailMessage}
+        autoHideDuration={4000}
+        message="Something went wrong, please try again!"
+        onClose={closeSnackBarHandler}
+        action={closeAction}
+      />
+    </>
   );
 };
 
